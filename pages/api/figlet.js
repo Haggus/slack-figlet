3227d77c48e731507c40309aba594712798404c6
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2019-10-31 08:11:02 +0100

    Add figlet renderer to figlet route

--- pages/api/figlet.js
+++ pages/api/figlet.js
@@ -1,5 +1,12 @@
+import figletRender from '../../lib/figlet-render';
+
 export default (req, res) => {
   res.setHeader('Content-Type', 'application/json');
+
+  if (!req.body.text) {
+    return res.json({ text: 'Error: Missing text' });
+  }
+
   res.statusCode = 200;
-  res.end(JSON.stringify({ hello: 'world' }));
+  res.end(JSON.stringify({ text: figletRender(req.body.text) }));
 };
